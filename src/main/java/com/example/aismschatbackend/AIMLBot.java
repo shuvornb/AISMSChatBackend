b01ed503f1ab2ccc7348b1bed8c331ec85a12c0e
trina <trinadutta2012@gmail.com>
2021-03-23 17:24:28 -0400

    Updated for activity 1

--- src/main/java/com/example/aismschatbackend/AIMLBot.java
+++ src/main/java/com/example/aismschatbackend/AIMLBot.java
@@ -3,10 +3,13 @@ package com.example.aismschatbackend;
 import org.alicebot.ab.Bot;
 import org.alicebot.ab.Chat;
 import org.alicebot.ab.configuration.BotConfiguration;
+
+import java.io.IOException;
 import java.util.Scanner;
+import java.io.FileWriter;
 
 public class AIMLBot {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Bot bot = new Bot(BotConfiguration.builder()
                 .name("alice")
                 .path("src/main/resources")
@@ -14,11 +17,18 @@ public class AIMLBot {
 
         Chat chatSession = new Chat(bot);
         Scanner sc = new Scanner(System.in);
+        System.out.println("Hello there! Type Hi to start this session.");
+        System.out.println("PLease avoid using '.' in your response");
+        FileWriter fw = new FileWriter("my-file.xml");
         while(true) {
+            System.out.print("User: ");
             String userStr = sc.nextLine();
-            if(userStr.equals("exit")) break;
+            fw.write("USER:"+userStr+"\n");
+            if (userStr.equals("END")) break;
             String botStr = chatSession.multisentenceRespond(userStr);
             System.out.println("Bot: " + botStr);
+            fw.write("BOT:"+botStr+" \n ");
         }
+        fw.close();
     }
 }
