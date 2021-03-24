e1225d8563a1b812c1ff1da104c7ba608f79bca8
trina <trinadutta2012@gmail.com>
2021-03-24 14:39:59 -0400

    Updated AIMLBot main file and replaced all terms "survey" Chatlogs get saved in a folder with timestamp as filename

--- src/main/java/com/example/aismschatbackend/AIMLBot.java
+++ src/main/java/com/example/aismschatbackend/AIMLBot.java
@@ -3,13 +3,26 @@ package com.example.aismschatbackend;
 import org.alicebot.ab.Bot;
 import org.alicebot.ab.Chat;
 import org.alicebot.ab.configuration.BotConfiguration;
+import org.w3c.dom.Attr;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.Scanner;
-import java.io.FileWriter;
+import java.io.File;
 
 public class AIMLBot {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ParserConfigurationException, TransformerException {
         Bot bot = new Bot(BotConfiguration.builder()
                 .name("alice")
                 .path("src/main/resources")
@@ -19,16 +32,63 @@ public class AIMLBot {
         Scanner sc = new Scanner(System.in);
         System.out.println("Hello there! Type Hi to start this session.");
         System.out.println("PLease avoid using '.' in your response");
-        FileWriter fw = new FileWriter("my-file.xml");
+        //create the directory
+        File theDir = new File("C:\\Users\\trina\\IdeaProjects\\AISMSChatBackend\\chatlog");
+        if (!theDir.exists()){
+            theDir.mkdirs();
+        }
+        LocalDateTime start_timestamp = LocalDateTime.now();
+        String start = start_timestamp.toString();
+
+        //XML FIle formatting
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+        Document doc = dBuilder.newDocument();
+
+        // root element
+        Element rootElement = doc.createElement("activity1");
+        doc.appendChild(rootElement);
+
+        // chat element
+        Element chat = doc.createElement("chat");
+        rootElement.appendChild(chat);
+
+        // setting attribute to element
+        Attr attr = doc.createAttribute("session");
+        attr.setValue("user_id");
+        chat.setAttributeNode(attr);
         while(true) {
             System.out.print("User: ");
             String userStr = sc.nextLine();
-            fw.write("USER:"+userStr+"\n");
+            // user_input element
+            Element user = doc.createElement("user");
+            Attr attrType = doc.createAttribute("string");
+            attrType.setValue("input");
+            user.setAttributeNode(attrType);
+            user.appendChild(doc.createTextNode(userStr));
+            chat.appendChild(user);
+
             if (userStr.equals("END")) break;
             String botStr = chatSession.multisentenceRespond(userStr);
             System.out.println("Bot: " + botStr);
-            fw.write("BOT:"+botStr+" \n ");
+            // user_input element
+            Element chatBot = doc.createElement("chatBot");
+            Attr attrType1 = doc.createAttribute("string");
+            attrType1.setValue("input");
+            chatBot.setAttributeNode(attrType1);
+            chatBot.appendChild(doc.createTextNode(botStr));
+            chat.appendChild(chatBot);
+
         }
-        fw.close();
+        LocalTime end_timestamp = LocalTime.now();
+        String end = end_timestamp.toString();
+        String fileName = start + end;
+        fileName = fileName.replace(':','-');
+        // write the content into xml file
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+        DOMSource source = new DOMSource(doc);
+        StreamResult result = new StreamResult(new File("C:\\Users\\trina\\IdeaProjects\\AISMSChatBackend\\chatlog\\"+fileName+".xml"));
+        transformer.transform(source, result);
     }
 }
