81917113f3f01e753eb271e3974fd65133789291
Md Shamim Seraj <shuvornb@gmail.com>
2021-03-25 11:59:59 -0400

    transfered chatbot to websocket handler

--- src/main/java/com/example/aismschatbackend/WebSocketHandler.java
+++ src/main/java/com/example/aismschatbackend/WebSocketHandler.java
@@ -4,16 +4,37 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.AbstractWebSocketHandler;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
 import java.io.IOException;
 
 public class WebSocketHandler extends AbstractWebSocketHandler {
+
     @Autowired
-    private ActivityService activityService = new ActivityService();
+    private ChatService chatService = new ChatService();
+    private long startTime;
+
+    public WebSocketHandler() throws ParserConfigurationException {
+    }
 
     @Override
-    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws IOException {
-        String msg = String.valueOf(message.getPayload());
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws IOException, TransformerException {
+        String msg = message.getPayload();
         System.out.println(msg);
-        session.sendMessage(new TextMessage(activityService.getNextQuestion()));
+        if(msg.equals("START")) {
+            startTime = System.currentTimeMillis();
+            chatService.insertParticipantStr(msg);
+            String botStr = chatService.chatSession.multisentenceRespond(msg);
+            chatService.insertBotStr(botStr);
+            session.sendMessage(new TextMessage(botStr));
+        }
+        else if(msg.equals("END")) chatService.dumpChatSession(startTime, System.currentTimeMillis());
+        else {
+            chatService.insertParticipantStr(msg);
+            String botStr = chatService.chatSession.multisentenceRespond(msg);
+            chatService.insertBotStr(botStr);
+            session.sendMessage(new TextMessage(botStr));
+        }
     }
 }
